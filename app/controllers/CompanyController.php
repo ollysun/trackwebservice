d38d2c9dd45fd3bb5f43fab754f870cfca01140a
Adeyemi Olaoye <yemi@cottacush.com>
2015-09-30 13:51:32 +0100

    return response

--- app/controllers/CompanyController.php
+++ app/controllers/CompanyController.php
@@ -77,7 +77,7 @@ class CompanyController extends ControllerBase
         if (!$primary_contact) {
             return $this->response->sendError(ResponseMessage::UNABLE_TO_CREATE_COMPANY_PRIMARY_CONTACT);
         }
-        $company->setPrimaryContactId($primary_contact->id);
+        $company->setPrimaryContactId($primary_contact->getId());
 
 
         if (isset($data['secondary_contact'])) {
@@ -88,7 +88,7 @@ class CompanyController extends ControllerBase
             if (!$secondary_contact) {
                 return $this->response->sendError(ResponseMessage::UNABLE_TO_CREATE_COMPANY_SECONDARY_CONTACT);
             }
-            $company->setSecContactId($secondary_contact->id);
+            $company->setSecContactId($secondary_contact->getId());
         } else {
             $secondary_contact = null;
             $company->setSecContactId(null);
@@ -99,11 +99,11 @@ class CompanyController extends ControllerBase
         }
 
 
-        $primary_contact_data['id'] = $primary_contact->id;
+        $primary_contact_data['id'] = $primary_contact->getId();
         $company->notifyContact($primary_contact_data);
 
         if (isset($data['secondary_contact'])) {
-            $data['secondary_contact']['id'] = $secondary_contact->id;
+            $data['secondary_contact']['id'] = $secondary_contact->getId();
             $company->notifyContact($data['secondary_contact']);
         }
 
@@ -240,24 +240,33 @@ class CompanyController extends ControllerBase
 
         $requestValidator = new RequestValidator($postData, $requiredFields);
         if (!$requestValidator->validateFields()) {
-            $this->response->send($requestValidator->printValidationMessage());
+            return $this->response->send($requestValidator->printValidationMessage());
         }
 
         $company = Company::findFirst($postData['company_id']);
         if (!$company) {
-            $this->response->sendError('Invalid company id');
+            return $this->response->sendError('Invalid company id');
+        }
+
+        if (UserAuth::findFirstByEmail($postData['email'])) {
+            return $this->response->sendError('User already exists');
         }
 
         if (!in_array($postData['role_id'], [Role::COMPANY_ADMIN, Role::COMPANY_OFFICER])) {
-            $this->response->sendError('Invalid company user role');
+            return $this->response->sendError('Invalid company user role');
         }
 
+        $postData['password'] = $this->auth->generateToken(6);
         $user = $company->createUser($postData['role_id'], $postData);
 
         if (!$user) {
-            $this->response->sendError('Could not create user');
+            return $this->response->sendError('Could not create user');
         }
 
-        $this->response->sendSuccess($user->toArray());
+        $postData['id'] = $user->getId();
+        $company->notifyContact($postData);
+
+        $userData = $user->toArray();
+        return $this->response->sendSuccess($userData);
     }
 } 
