0dc877b297259c161bcfa4e3f1d22d6b56e41d0d
Adegoke Obasa <goke@cottacush.com>
2015-10-13 22:05:06 +0100

    Refactored PickupRequest model, move with_company to getReqeusts

--- app/models/PickupRequest.php
+++ app/models/PickupRequest.php
@@ -65,6 +65,11 @@ class PickupRequest extends BaseModel
             ->from('PickupRequest');
         $columns = ['PickupRequest.*'];
 
+        if (in_array('with_company', $fetch_with)) {
+            $columns[] = 'Company.*';
+            $builder = $builder->innerJoin('Company', 'Company.id = PickupRequest.company_id');
+        }
+
         if (in_array('with_pickup_state', $fetch_with)) {
             $columns[] = 'PickupState.*';
             $builder = $builder->innerJoin('State', 'PickupState.id = PickupRequest.pickup_state_id', 'PickupState');
@@ -92,6 +97,10 @@ class PickupRequest extends BaseModel
         $requests = [];
         foreach ($result as $data) {
             $request = (property_exists($data, 'pickupRequest')) ? $data->pickupRequest->toArray() : $data->toArray();
+
+            if (in_array('with_company', $fetch_with)) {
+                $request['company'] = $data->company->getData();
+            }
 
             if (in_array('with_pickup_city', $fetch_with)) {
                 $request['pickup_city'] = $data->PickupCity->toArray();
@@ -168,11 +177,6 @@ class PickupRequest extends BaseModel
             ->from('PickupRequest');
         $columns = ['PickupRequest.*'];
 
-        if (in_array('with_company', $fetch_with)) {
-            $columns[] = 'Company.*';
-            $builder = $builder->innerJoin('Company', 'Company.id = PickupRequest.company_id');
-        }
-
         if (in_array('with_pickup_state', $fetch_with)) {
             $columns[] = 'PickupState.*';
             $builder = $builder->innerJoin('State', 'PickupState.id = PickupRequest.pickup_state_id', 'PickupState');
@@ -216,9 +220,6 @@ class PickupRequest extends BaseModel
 
         if (isset($result[0]->pickupRequest)) {
             $request = $result[0]->pickupRequest->getData();
-            if (in_array('with_company', $fetch_with)) {
-                $request['company'] = $result[0]->company->getData();
-            }
             if (in_array('with_pickup_city', $fetch_with)) {
                 $request['pickup_city'] = $result[0]->PickupCity->getData();
             }
