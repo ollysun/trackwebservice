3078deefbb4a2206eb1325ef0c9fa1a2ef01c92f
Abdul-Rahman Shitu <shituabdulrahman@gmail.com>
2015-10-18 09:53:23 +0100

    [CPTTK-332] - Added some bagging error messages in ResponseMessage

--- app/components/ResponseMessage.php
+++ app/components/ResponseMessage.php
@@ -87,9 +87,13 @@ class ResponseMessage
     const ZONE_MATRIX_NOT_EXIST = 'Zone matrix mapping does not exist';
 
     const NO_PARCEL_TO_BAG = 'No parcel to bag';
+    const NO_PARCEL_TO_REMOVE_FROM_BAG = 'No parcel to remove from bag';
     const PARCEL_NOT_BE_BAGGED = 'Parcel cannot be bagged';
     const PARCEL_ALREADY_BAGGED = 'Parcel is already bagged';
     const PARCEL_NOT_GOING_TO_BAG_LOC = 'Parcel not heading to bag\'s destination';
+    const BAG_DOES_NOT_EXIST = 'Bag does not exist';
+    const BAG_IN_MANIFEST = 'Bag is already in a manifest';
+    const PARCEL_NOT_IN_BAG = 'Parcel is not in bag';
 
     const CAN_ONLY_DELIVER_FROM_EC = 'Must be login to an EC to set parcels for delivery';
 
