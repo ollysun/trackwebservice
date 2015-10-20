885b91e205c31ef10e8ab502ad38d4aae011ae84
Adeyemi Olaoye <yemi@cottacush.com>
2015-10-20 12:34:20 +0100

    modify user auth alter sql

--- data/user_auth_alter_last_login.sql
+++ data/user_auth_alter_last_login.sql
@@ -1,1 +1,2 @@
-ALTER TABLE user_auth ADD COLUMN last_login_time TIMESTAMP DEFAULT 0 after modified_date;
+ALTER TABLE user_auth ADD COLUMN last_login_time INT DEFAULT 0 after modified_date;
+UPDATE  user_auth SET last_login_time = UNIX_TIMESTAMP();
