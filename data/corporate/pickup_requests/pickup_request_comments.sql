56ace56b564732031916ce032cb4a16ecef962ed
Adegoke Obasa <goke@cottacush.com>
2015-10-21 12:09:49 +0100

    Create Shipment and Pickup request comments tables

---
+++ data/corporate/pickup_requests/pickup_request_comments.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS pickup_request_comments;
+
+CREATE TABLE IF NOT EXISTS pickup_request_comments (
+  `id` INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `pickup_request_id` INT(11) NOT NULL,
+  `comment` TEXT NOT NULL,
+  `type` VARCHAR(25) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `modified_at` DATETIME NOT NULL,
+  `status` TINYINT(1) DEFAULT 1,
+  KEY k_pickup_request_id (pickup_request_id),
+  KEY k_type (`type`),
+  KEY k_status (`status`),
+  CONSTRAINT fk_src_sr_pickup_request_id FOREIGN KEY (pickup_request_id) REFERENCES pickup_requests(id)
+);
