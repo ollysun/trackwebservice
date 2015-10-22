85ac22e167a47cc53c08cdc9a90ff6439759e883
Adeyemi Olaoye <yemi@cottacush.com>
2015-10-22 13:57:51 +0100

    add email, name and phone number to move to delivered api

--- app/controllers/ParcelController.php
+++ app/controllers/ParcelController.php
@@ -806,6 +806,12 @@ class ParcelController extends ControllerBase
         $this->auth->allowOnly([Role::OFFICER, Role::DISPATCHER, Role::SWEEPER, Role::GROUNDSMAN]);
 
         $waybill_numbers = $this->request->getPost('waybill_numbers');
+
+        //get receipt information
+        $receiver_name = $this->request->getPost('receiver_name');
+        $receiver_phonenumber = $this->request->getPost('receiver_phone_number');
+        $receiver_email = $this->request->getPost('receiver_email');
+
         $admin_id = $this->auth->getPersonId();
 
         if (in_array(null, [$waybill_numbers, $admin_id])) {
@@ -863,6 +869,19 @@ class ParcelController extends ControllerBase
                     'Courier Plus',
                     $email
                 );
+            }
+
+            //create delivery receipt if receiver name and phone number was supplied
+            if (isset($receiver_name, $receiver_phonenumber)) {
+                if (!DeliveryReceipt::doesReceiptExist($waybill_number, DeliveryReceipt::RECEIPT_TYPE_RECEIVER_DETAIL)) {
+                    $data = ['waybill_number' => $waybill_number,
+                        'receipt_type' => DeliveryReceipt::RECEIPT_TYPE_RECEIVER_DETAIL,
+                        'delivered_by' => $this->auth->getPersonId(),
+                        'name' => $receiver_email,
+                        'phone_number' => $receiver_phonenumber,
+                        'email' => $receiver_email];
+                    DeliveryReceipt::add($data);
+                }
             }
 
             //TODO send email to operations team
