a34c085952d9f040b7e69b9760722372648e5cec
Adegoke Obasa <goke@cottacush.com>
2015-10-22 15:24:44 +0100

    - Create add method in CompanyBranch model

--- app/models/CompanyBranch.php
+++ app/models/CompanyBranch.php
@@ -6,6 +6,20 @@
 class CompanyBranch extends EagerModel
 {
 
+    /**
+     * Creates a new link between company and branch
+     * @author Adegoke Obasa <goke@cottacush.com>
+     * @param $companyId
+     * @param $branchId
+     * @return bool
+     */
+    public function add($companyId, $branchId)
+    {
+        $companyBranch = new CompanyBranch();
+        $companyBranch->company_id = $companyId;
+        $companyBranch->branch_id = $branchId;
+        return $companyBranch->save();
+    }
 
     /**
      * Returns an array that maps related models
