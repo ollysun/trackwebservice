d7b9dcba3b667de251a426a77113f2d8dfaf30d4
babatunde <otarubabatunde@gmail.com>
2015-11-09 00:02:37 +0100

    Allowing users to select reasons for return.

--- data/parcel/parcel_return.sql
+++ data/parcel/parcel_return.sql
@@ -2,5 +2,5 @@ CREATE TABLE `return_reasons` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `status_code` INT(11) NOT NULL,
   `meaning_of_status` VARCHAR(45) NOT NULL,
-  `useage_of_status` VARCHAR (45) NOT NULL
+  `usage_of_status` VARCHAR (45) NOT NULL
 );
