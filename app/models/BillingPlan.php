f7327f07c36645d9b8aebf21d8c1c9d457474a13
Adegoke Obasa <goke@cottacush.com>
2015-11-11 07:23:57 +0100

    Create migrations for new stored procs

--- app/models/BillingPlan.php
+++ app/models/BillingPlan.php
@@ -5,6 +5,7 @@ class BillingPlan extends \Phalcon\Mvc\Model
     const TYPE_WEIGHT = 1;
     const TYPE_ON_FORWARDING = 2;
     const TYPE_NUMBER = 3;
+    const TYPE_WEIGHT_AND_ON_FORWARDING = 4;
 
     /**
      *
