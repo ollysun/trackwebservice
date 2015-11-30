ae77f37beabb37cd30e5979b6bd63ea40654351d
Adegoke Obasa <goke@cottacush.com>
2015-11-30 14:16:15 +0100

    Create task to zero onforwarding charge for some companies

---
+++ app/tasks/ZeroOnforwardingChargesTask.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Changes all onforwarding charges for companies listed to zero
+ * Requires A list of companies in the data folder
+ * @author Adegoke Obasa <goke@cottacush.com>
+ */
+class ZeroOnforwardingChargesTask extends BaseTask
+{
+    /**
+     * Main Action
+     * @author Adegoke Obasa <goke@cottacush.com>
+     */
+    public function mainAction()
+    {
+        // Find company by name
+        $dataFileName = 'companies_zeroised_on_forwarding_charges.txt';
+        $companies = file_get_contents(dirname(__FILE__) . "/../../data/corporate/$dataFileName", "r");
+        $companies = explode("\n", $companies);
+
+        foreach ($companies as $company) {
+            $this->zeroiseOnforwarding($company);
+        }
+        // Use company_id to find billing plans for company
+
+    }
+
+    /**
+     *
+     * @author Adegoke Obasa <goke@cottacush.com>
+     * @param $companyName
+     */
+    private function zeroiseOnforwarding($companyName)
+    {
+        $company = Company::findFirstByName($companyName);
+
+        // If company exists
+        if ($company) {
+            /**
+             * @var Company $company
+             */
+            $companyId = $company->getId();
+            // Use company_id to find billing plans for company
+
+            /**
+             * @var \Phalcon\Mvc\Model\Resultset $billingPlans
+             */
+            $billingPlans = BillingPlan::find("company_id=$companyId");
+            if($billingPlans->count() > 0) {
+                // Billing plans exist, use billing plan id to zeroise onforwarding charges
+                $ids = [];
+                foreach($billingPlans as $billingPlan) {
+                    /**
+                     * @var BillingPlan $billingPlan
+                     */
+                    $ids[] = $billingPlan->getId();
+                }
+                $ids = implode(',', $ids);
+                $connection = (new BaseModel())->getWriteConnection();
+                $status = $connection->update('onforwarding_charge', ['amount', 'percentage'], [0, 0], "billing_plan_id IN ($ids)");
+                if(!$status) {
+                    $this->printToConsole("Unable to update onforwarding charge for *$companyName*");
+                }
+            } else {
+                // Company does not have a billing plan
+                $this->printToConsole("Company *{$company->getName()}* does not have any billing plans");
+            }
+        } else {
+            // Invalid company name
+            $this->printToConsole("Company with name *$companyName* does not exists");
+        }
+    }
+}
