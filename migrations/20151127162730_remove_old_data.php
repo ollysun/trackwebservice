c94bfbbab0efbf83a5f9cfc4b5b18d8599cbd5ea
Adeyemi Olaoye <yemi@cottacush.com>
2015-12-02 10:33:27 +0100

    Added request comments tables

--- migrations/20151127162730_remove_old_data.php
+++ migrations/20151127162730_remove_old_data.php
@@ -45,6 +45,38 @@ class RemoveOldData extends AbstractMigration
             $this->execute($sql);
         }
 
+        if (!$this->hasTable('shipment_request_comments')) {
+            $sql = 'CREATE TABLE IF NOT EXISTS shipment_request_comments (
+                  `id` INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                  `shipment_request_id` INT(11) NOT NULL,
+                  `comment` TEXT NOT NULL,
+                  `type` VARCHAR(25) NOT NULL,
+                  `created_at` DATETIME NOT NULL,
+                  `updated_at` DATETIME NOT NULL,
+                  `status` TINYINT(1) DEFAULT 1,
+                  KEY k_shipment_request_id (shipment_request_id),
+                  KEY k_type (`type`),
+                  KEY k_status (`status`),
+                  CONSTRAINT fk_src_sr_shipment_request_id FOREIGN KEY (shipment_request_id) REFERENCES shipment_requests(id));';
+            $this->execute($sql);
+        }
+
+        if (!$this->hasTable('shipment_request_comments')) {
+            $sql = 'CREATE TABLE IF NOT EXISTS pickup_request_comments (
+                  `id` INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                  `pickup_request_id` INT(11) NOT NULL,
+                  `comment` TEXT NOT NULL,
+                  `type` VARCHAR(25) NOT NULL,
+                  `created_at` DATETIME NOT NULL,
+                  `updated_at` DATETIME NOT NULL,
+                  `status` TINYINT(1) DEFAULT 1,
+                  KEY k_pickup_request_id (pickup_request_id),
+                  KEY k_type (`type`),
+                  KEY k_status (`status`),
+                  CONSTRAINT fk_src_sr_pickup_request_id FOREIGN KEY (pickup_request_id) REFERENCES pickup_requests(id));';
+            $this->execute($sql);
+        }
+
         if ($this->hasTable('parcel_teller')) {
             $this->dropTable('parcel_teller');
         }
