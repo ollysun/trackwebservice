b06bc623f544c2ee2ae6f6da66f63287176af883
Adegoke Obasa <goke@cottacush.com>
2015-12-03 12:11:11 +0100

    Add filters to getAll Invoice API

--- app/models/Invoice.php
+++ app/models/Invoice.php
@@ -1,9 +1,10 @@
 <?php
+use Phalcon\Mvc\Model\Query\BuilderInterface;
 
 /**
  * @author Adegoke Obasa <goke@cottacush.com>
  */
-class Invoice extends BaseModel
+class Invoice extends EagerModel
 {
 
     /**
@@ -53,18 +54,25 @@ class Invoice extends BaseModel
             ->from('Invoice');
 
         $columns = ['Invoice.*'];
+        $obj->setFetchWith($fetch_with)
+            ->joinWith($builder, $columns);
 
+        $builder = self::addFetchCriteria($builder, $filter_by);
         $builder->columns($columns)->offset($offset)->limit($count);
 
         $result = $builder->getQuery()->execute();
         $invoices = [];
         foreach ($result as $data) {
-            $invoices[] = $data->toArray();
+            $invoice = (property_exists($data, 'invoice')) ? $data->invoice->toArray() : $data->toArray();
+
+            $relatedRecords = $obj->loadRelatedModels($data, true);
+            $invoice = array_merge($invoice, $relatedRecords);
+
+            $invoices[] = $invoice;
         }
 
         return $invoices;
     }
-
 
 
     /**
@@ -85,12 +93,38 @@ class Invoice extends BaseModel
     /**
      * Adds filter params to builder
      * @author Adegoke Obasa <goke@cottacush.com>
-     * @param $builder
+     * @param BuilderInterface $builder
      * @param $filter_by
      * @return mixed
      */
     private static function addFetchCriteria($builder, $filter_by)
     {
+        if (isset($filter_by['status'])) {
+            $builder->andWhere('Invoice.status=:status:', ['status' => $filter_by['status']], ['status' => PDO::PARAM_STR]);
+        }
+
+        if (isset($filter_by['company_id'])) {
+            $builder->andWhere('Invoice.company_id=:company_id:', ['company_id' => $filter_by['company_id']], ['company_id' => PDO::PARAM_INT]);
+        }
+
         return $builder;
+    }
+
+    /**
+     * Returns an array that maps related models
+     * @author Adegoke Obasa <goke@cottacush.com>
+     * @return array
+     */
+    public function getFetchWithMap()
+    {
+        return [
+            [
+                'field' => 'company',
+                'model_name' => 'Invoice',
+                'ref_model_name' => 'Company',
+                'foreign_key' => 'company_id',
+                'reference_key' => 'id'
+            ]
+        ];
     }
 }
