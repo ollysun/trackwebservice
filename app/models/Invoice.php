38ac44c672020c457c0790d8fe4b8460a4b27911
Adegoke Obasa <goke@cottacush.com>
2015-12-07 20:27:00 +0100

    Change invoice prefix to IN

--- app/models/Invoice.php
+++ app/models/Invoice.php
@@ -33,7 +33,7 @@ class Invoice extends EagerModel
      */
     public static function generateInvoiceNumber()
     {
-        $invoiceNumber = 'CP-' . date('Ymd-His') . '-' . rand(0, 10);
+        $invoiceNumber = 'IN-' . date('Ymd-His') . '-' . rand(0, 10);
         return $invoiceNumber;
     }
 
