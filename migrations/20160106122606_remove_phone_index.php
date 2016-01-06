ac9ef3a5ec70aa8b2ba5d8f4d8f7b919a73d2a71
Olawale Lawal <wale@cottacush.com>
2016-01-06 14:25:03 +0100

    Added `down()`, switched from `change()` to `up()`

--- migrations/20160106122606_remove_phone_index.php
+++ migrations/20160106122606_remove_phone_index.php
@@ -5,28 +5,16 @@ use Phinx\Migration\AbstractMigration;
 class RemovePhoneIndex extends AbstractMigration
 {
     /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
+     * Remove the `phone` number index
+     * @author Olawale Lawal <wale@cottacush.com>
      */
-    public function change()
+    public function up()
     {
         $this->table('user')->removeIndex(array('phone'));
+    }
+
+    public function down()
+    {
+        echo "This migration cannot be reversed";
     }
 }
