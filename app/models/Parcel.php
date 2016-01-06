ddfae6e02c6f520b2e10efc02c15e041f35f8234
Olawale Lawal <wale@cottacush.com>
2016-01-06 14:14:14 +0100

    PR conflicts

--- app/models/Parcel.php
+++ app/models/Parcel.php
@@ -23,7 +23,6 @@ class Parcel extends \Phalcon\Mvc\Model
     const SQL_DELETE_LINKAGE = 'DELETE FROM linked_parcel WHERE parent_id = :parent_id';
     const SQL_UPDATE_SUBS = 'UPDATE parcel SET from_branch_id = :from_branch_id, to_branch_id = :to_branch_id, `status` = :status, modified_date = :modified_date WHERE id IN (SELECT child_id FROM linked_parcel WHERE parent_id = :parent_id)';
 
-
     /**
      *
      * @var integer
