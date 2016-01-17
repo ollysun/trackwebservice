3733002c951c4ce7537a4363e4c9c9740b615644
Adeyemi Olaoye <yemi@cottacush.com>
2016-01-17 19:33:14 +0100

    generate bulk waybill pdf to local file

--- app/jobs/BulkWaybillPrintingJob.php
+++ app/jobs/BulkWaybillPrintingJob.php
@@ -1,5 +1,7 @@
 <?php
 use mikehaertl\wkhtmlto\Pdf;
+use Phalcon\Mvc\Model\Resultset;
+use Picqer\Barcode\BarcodeGeneratorHTML;
 
 /**
  * Class BulkWaybillPrintingJob
@@ -7,6 +9,21 @@ use mikehaertl\wkhtmlto\Pdf;
  */
 class BulkWaybillPrintingJob extends BaseJob
 {
+    private $parcelTypes;
+    private $shippingTypes;
+
+    public function onStart()
+    {
+        parent::onStart();
+        /** @var Resultset $parcelTypes */
+        $parcelTypes = ParcelType::find();
+        $this->parcelTypes = $this->getDataMap(['id' => 'name'], $parcelTypes->toArray());
+
+        /** @var Resultset $shipmentTypes */
+        $shippingTypes = ShippingType::find();
+        $this->shippingTypes = $this->getDataMap(['id' => 'name'], $shippingTypes->toArray());
+    }
+
     /**
      * execute current job
      * @author Adeyemi Olaoye <yemi@cottacush.com>
@@ -23,8 +40,8 @@ class BulkWaybillPrintingJob extends BaseJob
 
         $pdf = new Pdf();
         $waybill_layout = file_get_contents(dirname(__DIR__) . '/html/bulk_waybill_layout.html');
-        $pdf_content = Util::replaceTemplate($waybill_layout, ['content' => $waybills_html]);
-        $pdf->addPage($pdf_content);
+        $html_content = Util::replaceTemplate($waybill_layout, ['content' => $waybills_html]);
+        $pdf->addPage($html_content);
         $pdf->saveAs(dirname(__DIR__) . '/html/bulk_waybill_layout.pdf');
         exit;
     }
@@ -44,7 +61,30 @@ class BulkWaybillPrintingJob extends BaseJob
         if (!$parcel) {
             return '';
         }
-        $params = [
+
+        $placeholderValues = $this->getPlaceholderValues($parcel);
+        $waybill_template = Util::replaceTemplate($waybill_template, $placeholderValues);
+        foreach ($copies as $i => $copy) {
+            $waybill_html .= Util::replaceTemplate($waybill_template, ['copy' => $copy]);
+            if ($i == 0 || $i == 2) {
+                $waybill_html .= '<div class="waybill-divider"></div>';
+            }
+        }
+
+        return $waybill_html;
+    }
+
+    /**
+     * Get Placeholder values
+     * @author Adeyemi Olaoye <yemi@cottacush.com>
+     * @param $parcel
+     * @return array
+     */
+    private function getPlaceholderValues($parcel)
+    {
+        $generator = new BarcodeGeneratorHTML();
+        $barCodeData = $generator->getBarcode($parcel['waybill_number'], BarcodeGeneratorHTML::TYPE_CODE_128, 2, 78);
+        return [
             'waybill_number' => $parcel['waybill_number'],
             'sender_name' => $parcel['sender']['firstname'] . ' ' . $parcel['sender']['lastname'],
             'sender_address' => $parcel['sender_address']['street_address1'] .
@@ -69,18 +109,47 @@ class BulkWaybillPrintingJob extends BaseJob
             'no_of_package' => $parcel['no_of_package'],
             'weight' => ($parcel['qty_metrics'] == Parcel::QTY_METRICS_WEIGHT) ? Util::formatWeight($parcel['weight']) . 'Kg' : '',
             'pieces' => ($parcel['qty_metrics'] == Parcel::QTY_METRICS_PIECES) ? $parcel['weight'] : '',
-            'service_types' => '',
-            'parcel_type' => '',
+            'service_types' => $this->getServiceTypeHtml($parcel['shipping_type']),
+            'parcel_type' => $this->shippingTypes[$parcel['parcel_type']],
             'cod_yes' => (($parcel['cash_on_delivery'] == '1') ? 'is-active' : ''),
             'cod_no' => (($parcel['cash_on_delivery'] == '1') ? '' : 'is-active'),
-            'other_info' => $parcel['other_info']
+            'other_info' => $parcel['other_info'],
+            'barcode_data' => $barCodeData
         ];
+    }
 
-        $waybill_template = Util::replaceTemplate($waybill_template, $params);
-        foreach ($copies as $copy) {
-            $waybill_html .= Util::replaceTemplate($waybill_template, ['copy' => $copy]);
+    /**
+     * Map data as give in map
+     * @author Adeyemi Olaoye <yemi@cottacush.com>
+     * @param $map
+     * @param $array
+     * @return array
+     */
+    private function getDataMap($map, $array)
+    {
+        $dataMap = [];
+        foreach ($map as $key => $value) {
+            foreach ($array as $element) {
+                $dataMap[$element[$key]] = $element[$value];
+            }
         }
 
-        return $waybill_html;
+        return $dataMap;
+    }
+
+    /**
+     * Get service type html
+     * @author Adeyemi Olaoye <yemi@cottacush.com>
+     * @param $service_type
+     * @return string
+     */
+    public function getServiceTypeHtml($service_type)
+    {
+        $serviceTypeHtml = '';
+        foreach ($this->shippingTypes as $id => $name) {
+            $class = ($service_type == $id) ? 'service-type__inner is-active' : 'service-type__inner';
+            $serviceTypeHtml .= "<div class='{$class}'><span>" . ucwords($name) . "</span></div>";
+        }
+        return $serviceTypeHtml;
     }
 }
