7f0f5d2763ca2ea0241f7d957a1d5922284c55fa
tunde <tnde@babatunde>
2016-02-01 11:51:09 +0100

    Allow sweeper/Dispatcher calculate billing

--- app/controllers/ZoneController.php
+++ app/controllers/ZoneController.php
@@ -346,7 +346,7 @@ class ZoneController extends ControllerBase
 
     public function calcBillingAction()
     {
-        $this->auth->allowOnly([Role::ADMIN, Role::OFFICER]);
+        $this->auth->allowOnly([Role::ADMIN, Role::OFFICER,Role::SWEEPER,Role::DISPATCHER]);
 
         $from_branch_id = $this->request->getPost('from_branch_id');
         $to_branch_id = $this->request->getPost('to_branch_id');
