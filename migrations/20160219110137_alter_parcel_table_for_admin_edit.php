db7f9064f62847f45ae9ff34b13e921671db6250
tunde <tnde@babatunde>
2016-02-19 23:26:14 +0100

    Use phinx methods.

--- migrations/20160219110137_alter_parcel_table_for_admin_edit.php
+++ migrations/20160219110137_alter_parcel_table_for_admin_edit.php
@@ -6,9 +6,13 @@ class AlterParcelTableForAdminEdit extends AbstractMigration
 {
     public function change()
     {
-        if($this->hasTable('parcel')){
-            $sql = file_get_contents(dirname(__FILE__) . '/../data/parcel/parcel_admin_edit_alter.sql');
-            $this->execute($sql);
-        }
+        $this->table('parcel')
+            ->addColumn('insurance','decimal',['null' => true])
+            ->addColumn('storage_demurrage','decimal',['null' => true])
+            ->addColumn('handling_charge','decimal',['null' => true])
+            ->addColumn('duty_charge','decimal',['null' => true])
+            ->addColumn('cost_of_crating','decimal',['null' => true])
+            ->addColumn('others','decimal',['null' => true])
+            ->update();
     }
 }
