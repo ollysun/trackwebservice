fdf0ab3cb3faab9575070a6fb208aeb4f078f80c
Adeyemi Olaoye <yemi@cottacush.com>
2016-03-02 10:27:40 +0100

    add migration to change status of failed jobs

---
+++ migrations/20160302082228_update_bulk_parcel_creation_job_status.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * Class UpdateBulkParcelCreationJobStatus
+ * @author Adeyemi Olaoye <yemi@cottacush.com>
+ */
+class UpdateBulkParcelCreationJobStatus extends AbstractMigration
+{
+
+    public function change()
+    {
+       $jobIds =  $this->fetchAll("SELECT DISTINCT j.id FROM jobs j LEFT JOIN bulk_shipment_job_details bsjd ON (j.id = bsjd.job_id AND bsjd.status = 'success') WHERE bsjd.id IS NULL AND j.queue = 'bulk_shipment_creation'");
+
+        $jobIds = array_column($jobIds, 'id');
+
+        $this->execute("UPDATE jobs SET `status` = 'failed' WHERE id IN (".implode(',', $jobIds).")");
+    }
+}
