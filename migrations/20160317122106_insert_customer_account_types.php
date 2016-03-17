43190e778077259e075be1356838ca1e6b1a6345
tunde <tnde@babatunde>
2016-03-17 19:26:17 +0100

    Insert Corporates Account type.

---
+++ migrations/20160317122106_insert_customer_account_types.php
@@ -0,0 +1,15 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class InsertCustomerAccountTypes extends AbstractMigration
+{
+
+    public function change()
+    {
+
+        $account_types = [['M', 'ECR', 'Ecommerce Merchant on Credit'], ['CR', 'CCC', 'Corporate Credit client'],
+            ['CA', 'CSH', 'Cash Client'], ['CAM', 'ECS', 'Ecommerce Merchant on Cash']];
+        $this->table('corporate_account_type')->insert(['code', 'acronym', 'name'], $account_types)->update();
+    }
+}
