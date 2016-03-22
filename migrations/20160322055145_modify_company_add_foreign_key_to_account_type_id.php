699247a7eabb19fe1bf5f1589ca6c759c8976e00
tunde <tnde@babatunde>
2016-03-22 12:29:16 +0100

    Log corporate_account_type update report

--- migrations/20160322055145_modify_company_add_foreign_key_to_account_type_id.php
+++ migrations/20160322055145_modify_company_add_foreign_key_to_account_type_id.php
@@ -8,20 +8,21 @@ class ModifyCompanyAddForeignKeyToAccountTypeId extends AbstractMigration
     {
         $companies_account_types = file_get_contents(dirname(__FILE__) . '/../data/corporate/change_corporate_account_type', 'r');
         $companies_account_types = explode("\n", $companies_account_types);
-        $file = fopen('public/upload_customer_account_type.csv', 'a');
+        $file = fopen(dirname(__FILE__) . 'public/upload_customer_account_type.csv', 'a');
         foreach ($companies_account_types as $companies_account_type) {
             $corporates_array = str_getcsv($companies_account_type);
             $company_id = $corporates_array[0];
             $account_type_code = $corporates_array[5];
+            $company_name = $corporates_array[1];
             $account_type_code = preg_replace('/\s+/', '', $account_type_code);
             $account_type_code = str_replace("-", "–", $account_type_code);
             $account_type_code = explode("–", $account_type_code);
             $account_type_code = $account_type_code[0];
             $result = $this->execute("UPDATE company cp,corporate_account_type atype SET cp.account_type_id = atype.id WHERE atype.code = '$account_type_code' AND cp.id = $company_id");
-            if($result > 0){
-                fputcsv($file,[$corporates_array[1], 'Succesful']);
-            }else{
-                fputcsv($file,[$corporates_array[1], 'was not Succesful']);
+            if ($result > 0) {
+                fputcsv($file, [$company_name, 'was updated Successfully']);
+            } else {
+                fputcsv($file, [$company_name, ' was not updated Successfully']);
             }
         }
         $this->execute('UPDATE company cp,corporate_account_type atype SET cp.account_type_id = atype.id WHERE atype.code = "CA" AND cp.account_type_id = 0 ');
