34ce4e0bc4ba275dd6f82091681941d7411d9669
tunde <tnde@babatunde>
2016-03-22 13:35:48 +0100

    Code Clean Up.

--- migrations/20160322055145_modify_company_add_foreign_key_to_account_type_id.php
+++ migrations/20160322055145_modify_company_add_foreign_key_to_account_type_id.php
@@ -8,17 +8,16 @@ class ModifyCompanyAddForeignKeyToAccountTypeId extends AbstractMigration
     {
         $companies_account_types = file_get_contents(dirname(__FILE__) . '/../data/corporate/change_corporate_account_type', 'r');
         $companies_account_types = explode("\n", $companies_account_types);
-        $file = fopen(dirname(__FILE__) . 'public/upload_customer_account_type.csv', 'a');
+        $file = fopen('public/upload_customer_account_type.csv', 'a');
         foreach ($companies_account_types as $companies_account_type) {
             $corporates_array = str_getcsv($companies_account_type);
-            $company_id = $corporates_array[0];
             $account_type_code = $corporates_array[5];
-            $company_name = $corporates_array[1];
+            $company_name = addslashes($corporates_array[1]);
             $account_type_code = preg_replace('/\s+/', '', $account_type_code);
             $account_type_code = str_replace("-", "–", $account_type_code);
             $account_type_code = explode("–", $account_type_code);
             $account_type_code = $account_type_code[0];
-            $result = $this->execute("UPDATE company cp,corporate_account_type atype SET cp.account_type_id = atype.id WHERE atype.code = '$account_type_code' AND cp.id = $company_id");
+            $result = $this->execute("UPDATE company cp,corporate_account_type atype SET cp.account_type_id = atype.id WHERE atype.code = '$account_type_code' AND cp.name = '$company_name'");
             if ($result > 0) {
                 fputcsv($file, [$company_name, 'was updated Successfully']);
             } else {
@@ -26,6 +25,6 @@ class ModifyCompanyAddForeignKeyToAccountTypeId extends AbstractMigration
             }
         }
         $this->execute('UPDATE company cp,corporate_account_type atype SET cp.account_type_id = atype.id WHERE atype.code = "CA" AND cp.account_type_id = 0 ');
-        $this->table('company')->addForeignKey('account_type_id', 'corporate_account_type', 'id');
+        $this->execute('alter table company add FOREIGN key (account_type_id) REFERENCES corporate_account_type(id)');
     }
 }
