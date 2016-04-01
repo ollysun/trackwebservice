ba9d29e93066a02368d7e3bd8a9827d12d64ae48
Adeyemi Olaoye <yemi@cottacush.com>
2016-04-01 13:36:15 +0100

    add slack notifications for workers

--- deploy.php
+++ deploy.php
@@ -63,7 +63,7 @@ task('cleanup', function () {
 
 
 task('workers:stop', function () {
-    run('cd {{deploy_path}}/current && BEANSTALKD_HOST=localhost BEANSTALKD_PORT=11300 TNT_DB_HOST={{PHINX_DBHOST}} TNT_DB_USERNAME={{PHINX_DBUSER}} TNT_DB_PASSWORD={{PHINX_DBPASS}} TNT_DBNAME={{PHINX_DBNAME}} php app/cli.php worker stop ParcelCreationWorker > > {{deploy_path}}/current/app/logs/parcel_creation_worker.log');
+    run('cd {{deploy_path}}/current && BEANSTALKD_HOST=localhost BEANSTALKD_PORT=11300 TNT_DB_HOST={{PHINX_DBHOST}} TNT_DB_USERNAME={{PHINX_DBUSER}} TNT_DB_PASSWORD={{PHINX_DBPASS}} TNT_DBNAME={{PHINX_DBNAME}} php app/cli.php worker stop ParcelCreationWorker >  {{deploy_path}}/current/app/logs/parcel_creation_worker.log');
     run('cd {{deploy_path}}/current && BEANSTALKD_HOST=localhost BEANSTALKD_PORT=11300 TNT_DB_HOST={{PHINX_DBHOST}} TNT_DB_USERNAME={{PHINX_DBUSER}} TNT_DB_PASSWORD={{PHINX_DBPASS}} TNT_DBNAME={{PHINX_DBNAME}} php app/cli.php worker stop WaybillPrintingWorker > {{deploy_path}}/current/app/logs/waybill_printing_worker.log');
     run('cd {{deploy_path}}/current && BEANSTALKD_HOST=localhost BEANSTALKD_PORT=11300 TNT_DB_HOST={{PHINX_DBHOST}} TNT_DB_USERNAME={{PHINX_DBUSER}} TNT_DB_PASSWORD={{PHINX_DBPASS}} TNT_DBNAME={{PHINX_DBNAME}} php app/cli.php worker stop InvoicePrintingWorker > {{deploy_path}}/current/app/logs/invoice_printing_worker.log');
 });
@@ -82,9 +82,6 @@ task('workers:restart', [
 ]);
 
 
-after('deploy', 'workers:restart');
-
-
 //slack tasks
 task('slack:before_deploy', function () {
     postToSlack('Starting deploy on ' . env('server.name') . '...');
@@ -94,11 +91,35 @@ task('slack:after_deploy', function () {
     postToSlack('Deploy to ' . env('server.name') . ' done');
 });
 
+task('slack:before_workers_stop', function () {
+    postToSlack('Stopping Workers on ' . env('server.name') . '...');
+});
+
+task('slack:after_workers_stop', function () {
+    postToSlack('Workers stopped successfully');
+});
+
+task('slack:before_workers_start', function () {
+    postToSlack('Starting Workers on ' . env('server.name') . '...');
+});
+
+task('slack:after_workers_start', function () {
+    postToSlack('Workers started successfully');
+});
+
+
 function postToSlack($message)
 {
     runLocally('curl -s -S -X POST --data-urlencode payload="{\"channel\": \"#courier_plus\", \"username\": \"Tnt-Service Release Bot\", \"text\": \"' . $message . '\"}" https://hooks.slack.com/services/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');
 }
 
+after('workers:start', 'slack:after_workers_start');
+before('workers:start', 'slack:before_workers_start');
+
+after('workers:stop', 'slack:after_workers_stop');
+before('workers:stop', 'slack:before_workers_stop');
+
+after('deploy', 'workers:restart');
 before('deploy', 'slack:before_deploy');
 after('deploy', 'slack:after_deploy');
 
