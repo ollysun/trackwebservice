5430bcf1b9551638383366415e1e099d12091479
tunde <tnde@babatunde>
2016-04-05 15:19:19 +0100

    Allow from branch to mark a parcel as returned

--- app/controllers/ParcelController.php
+++ app/controllers/ParcelController.php
@@ -1265,7 +1265,7 @@ class ParcelController extends ControllerBase
             } else if ($parcel->getStatus() == Status::PARCEL_RETURNED) {
                 $bad_parcel[$waybill_number] = ResponseMessage::PARCEL_ALREADY_DELIVERED;
                 continue;
-            } else if ($parcel->getToBranchId() != $parcel->getCreatedBranchId() || (in_array($admin->getRoleId(), [Role::OFFICER]) && $admin->getBranchId() != $parcel->getToBranchId())) {
+            } else if ($parcel->getToBranchId() != $parcel->getCreatedBranchId() || (in_array($admin->getRoleId(), [Role::OFFICER]) && ($admin->getBranchId() != $parcel->getToBranchId() || $admin->getBranchId() != $parcel->getFromBranchId()))) {
                 $bad_parcel[$waybill_number] = ResponseMessage::PARCEL_WRONG_DESTINATION;
                 continue;
             }
