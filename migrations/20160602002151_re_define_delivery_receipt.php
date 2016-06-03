55e6361232d33d7fb0d201530cb392d9b7cb7d93
Tunde Otaru <tunde@cottacush.com>
2016-06-03 09:10:51 +0100

    Remove foreign key

--- migrations/20160602002151_re_define_delivery_receipt.php
+++ migrations/20160602002151_re_define_delivery_receipt.php
@@ -6,10 +6,9 @@ class ReDefineDeliveryReceipt extends AbstractMigration
 {
     public function change()
     {
-        $this->table('delivery_receipts')->addIndex('waybill_number')->update();;
-        $this->table('delivery_receipts')->addIndex('created_at')->update();
-        $this->table('delivery_receipts')->addIndex('delivered_at')->update();
-        $this->table('delivery_receipts')->addIndex('email')->update();
-        $this->table('delivery_receipts')->addForeignKey('waybill_number', 'parcel', 'waybill_number', ['constraint' => 'fk_delivery_receipts_parcel_waybill_number'])->update();
+        $this->table('delivery_receipts')->addIndex('waybill_number', ['name' => 'k_delivery_receipts_waybill_number'])->update();
+        $this->table('delivery_receipts')->addIndex('created_at', ['name' => 'k_delivery_receipts_created_at'])->update();
+        $this->table('delivery_receipts')->addIndex('delivered_at', ['name' => 'k_delivery_receipts_delivered_at'])->update();
+        $this->table('delivery_receipts')->addIndex('email', ['name' =>'k_delivery_receipts_email'])->update();
     }
 }
