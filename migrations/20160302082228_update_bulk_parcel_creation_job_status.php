4e9d993be3777ed7f516ba894e9c34198eec7340
Adeyemi Olaoye <yemi@cottacush.com>
2016-06-28 19:52:29 +0100

    Fix issue with update bulk parcel creation job status migration

--- migrations/20160302082228_update_bulk_parcel_creation_job_status.php
+++ migrations/20160302082228_update_bulk_parcel_creation_job_status.php
@@ -11,10 +11,15 @@ class UpdateBulkParcelCreationJobStatus extends AbstractMigration
 
     public function change()
     {
-       $jobIds =  $this->fetchAll("SELECT DISTINCT j.id FROM jobs j LEFT JOIN bulk_shipment_job_details bsjd ON (j.id = bsjd.job_id AND bsjd.status = 'success') WHERE bsjd.id IS NULL AND j.queue = 'bulk_shipment_creation'");
+        $jobIds = $this->fetchAll(
+            "SELECT DISTINCT j.id FROM jobs j LEFT JOIN bulk_shipment_job_details bsjd 
+            ON (j.id = bsjd.job_id AND bsjd.status = 'success') WHERE bsjd.id IS NULL 
+            AND j.queue = 'bulk_shipment_creation'"
+        );
 
-        $jobIds = array_column($jobIds, 'id');
-
-        $this->execute("UPDATE jobs SET `status` = 'failed' WHERE id IN (".implode(',', $jobIds).")");
+        if ($jobIds) {
+            $jobIds = array_column($jobIds, 'id');
+            $this->execute("UPDATE jobs SET `status` = 'failed' WHERE id IN (" . implode(',', $jobIds) . ")");
+        }
     }
 }
