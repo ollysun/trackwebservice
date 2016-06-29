6d2ac49079fcef10864c387f7f7e4cedf271b8a4
Adeyemi Olaoye <yemi@cottacush.com>
2016-06-29 11:42:07 +0100

    Switch user auth to packagist

--- migrations/20160317122106_insert_customer_account_types.php
+++ migrations/20160317122106_insert_customer_account_types.php
@@ -6,8 +6,11 @@ class InsertCustomerAccountTypes extends AbstractMigration
 {
     public function change()
     {
-        $account_types = [['M', 'ECR', 'Ecommerce Merchant on Credit'], ['CR', 'CCC', 'Corporate Credit client'],
-            ['CA', 'CSH', 'Cash Client'], ['CAM', 'ECS', 'Ecommerce Merchant on Cash']];
-        $this->table('corporate_account_type')->insert(['code', 'acronym', 'name'], $account_types)->update();
+        $account_types = [
+            ['code' => 'M', 'acronym' => 'ECR', 'name' => 'Ecommerce Merchant on Credit'],
+            ['code' => 'CR', 'acronym' => 'CCC', 'name' => 'Corporate Credit client'],
+            ['code' => 'CA', 'acronym' => 'CSH', 'name' => 'Cash Client'],
+            ['code' => 'CAM', 'acronym' => 'ECS', 'name' => 'Ecommerce Merchant on Cash']];
+        $this->table('corporate_account_type')->insert($account_types)->update();
     }
 }
