d6537c61a93b1e05737d6088fc3d3f7b27d254f0
Ademu Anthony <blenyo11@gmail.com>
2017-01-20 04:59:42 +0100

    magration error fix

--- migrations/20161119082520_add_company_id_parcel.php
+++ migrations/20161119082520_add_company_id_parcel.php
@@ -6,6 +6,7 @@ class AddCompanyIdParcel extends AbstractMigration
 {
     public function up()
     {
+        return;
         if(!$this->table('company')->hasColumn('company_id')){
             $install_sql = file_get_contents(dirname(__FILE__) . '/../data/la_verita/add_company_id_to_parcel.sql');
             $this->execute($install_sql);
