1151f10997c8abce102c98aa1c0b117100b7ac78
Ademu Anthony <blenyo11@gmail.com>
2017-01-20 05:02:29 +0100

    migration bug fix - option table

--- migrations/20161119082520_add_company_id_parcel.php
+++ migrations/20161119082520_add_company_id_parcel.php
@@ -6,7 +6,6 @@ class AddCompanyIdParcel extends AbstractMigration
 {
     public function up()
     {
-        return;
         if(!$this->table('company')->hasColumn('company_id')){
             $install_sql = file_get_contents(dirname(__FILE__) . '/../data/la_verita/add_company_id_to_parcel.sql');
             $this->execute($install_sql);
