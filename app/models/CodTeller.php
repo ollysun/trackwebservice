3484e831ff87b3318240e80322331c2cebbaba30
Ademu Anthony <blenyo11@gmail.com>
2017-01-20 11:39:37 +0100

    add teller bug fix

--- app/models/CodTeller.php
+++ app/models/CodTeller.php
@@ -510,9 +510,9 @@ class CodTeller extends \Phalcon\Mvc\Model
         $data = $builder->getQuery()->execute(['id' => $id]);
         if (count($data) == 0) return false;
 
-        dd($data[0]->codTeller);
         $result = $data[0]->codTeller->getData();
 
+        dd($result);
         $result['parcel'] = $data[0]->parcel->getData();
 
         return $result;
