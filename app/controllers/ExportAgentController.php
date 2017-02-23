5cdca488a4c3536e7df0ce5c1061fcde26b49be8
hintact-platform <raphaelint@gmail.com>
2017-02-23 12:11:30 +0100

    Exported Parcel Service

---
+++ app/controllers/ExportAgentController.php
@@ -0,0 +1,13 @@
+<?php
+
+class ExportedAgentController extends ControllerBase {
+
+    public function getAllAction(){
+        $this->auth->allowOnly(Role::ADMIN);
+
+        $agents = ExportAgent::find()->toArray();
+        //if($with_staff) $fetch_with['with_staff'] = true;
+
+        return $this->response->sendSuccess($agents);
+    }
+}
