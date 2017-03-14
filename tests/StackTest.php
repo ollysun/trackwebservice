e9804e66a723218f7d8a2c04e28512779d228f38
Ademu Anthony <blenyo11@gmail.com>
2017-03-14 14:35:28 +0100

    bm report download

---
+++ tests/StackTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Kalu
+ * Date: 14/03/2017
+ * Time: 05:22
+ */
+use PHPUnit_Framework_TestCase as TestCase;
+
+class StackTest extends TestCase
+{
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+}
