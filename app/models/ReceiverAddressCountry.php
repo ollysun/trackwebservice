b1cd7237f3e2aff7c875a92ae6e735d1d48c43f6
Ademu Anthony <blenyo11@gmail.com>
2017-04-14 18:09:18 +0100

    country in sender/receiver address

---
+++ app/models/ReceiverAddressCountry.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ELACHI
+ * Date: 4/14/2017
+ * Time: 6:04 PM
+ */
+class ReceiverAddressCountry extends Country
+{
+    function getSource(){
+        return "country";
+    }
+}
---
+++ app/models/SenderAddressCountry.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ELACHI
+ * Date: 4/14/2017
+ * Time: 6:05 PM
+ */
+class SenderAddressCountry extends Country
+{
+    function getSource(){
+      return "country";
+    }
+}
